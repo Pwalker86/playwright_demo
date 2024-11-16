20e639dcab5f8e8a80b4cac1dfdd002c6c11a826
Phil Walker <phil1586@gmail.com>
2024-11-15 17:14:32 -0700

    get a quote tests

--- tests/firstmile.spec.js
+++ tests/firstmile.spec.js
@@ -1,14 +1,97 @@
-const { test, expect } = require("@playwright/test");
+import { test, expect } from "@playwright/test";
 
 const firstMileHomePage = "https://www.firstmile.com/";
 const trackingURL = "https://track.firstmile.com/";
 const badTrackinNumbers = ["1234", "5678", "91011"];
 
-test("has title", async ({ page }) => {
-  await page.goto(firstMileHomePage);
+test.describe("landing", () => {
+  test("Home page loads", async ({ page }) => {
+    await page.goto(firstMileHomePage);
+    await expect(page).toHaveTitle(/FirstMile/);
+  });
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/FirstMile/);
+  test("Accept Cookies", async ({ page }) => {
+    await page.goto(firstMileHomePage);
+    await expect(page.locator("#hs-eu-cookie-confirmation")).toBeVisible();
+    await page.getByRole("button", { name: "Accept Cookies" }).click();
+    await expect(page.locator("#hs-eu-cookie-confirmation")).not.toBeVisible();
+  });
+
+  test("Request a Quote", async ({ page }) => {
+    await page.goto(firstMileHomePage);
+    const page1Promise = page.waitForEvent("popup");
+    await page
+      .getByRole("banner")
+      .getByRole("link", { name: "Get a Quote" })
+      .click();
+    const page1 = await page1Promise;
+    await expect(
+      page1
+        .locator('iframe[title="Form 0"]')
+        .contentFrame()
+        .getByText("First Name*")
+    ).toBeVisible();
+    await expect(
+      page1
+        .locator('iframe[title="Form 0"]')
+        .contentFrame()
+        .getByLabel("First Name*")
+    ).toBeVisible();
+    await expect(
+      page1
+        .locator('iframe[title="Form 0"]')
+        .contentFrame()
+        .getByLabel("Last Name*")
+    ).toBeVisible();
+    await expect(
+      page1
+        .locator('iframe[title="Form 0"]')
+        .contentFrame()
+        .getByLabel("Business Email*")
+    ).toBeVisible();
+    await expect(
+      page1
+        .locator('iframe[title="Form 0"]')
+        .contentFrame()
+        .getByLabel("Phone Number*")
+    ).toBeVisible();
+    await expect(
+      page1
+        .locator('iframe[title="Form 0"]')
+        .contentFrame()
+        .getByLabel("Website URL*")
+    ).toBeVisible();
+    await expect(
+      page1
+        .locator('iframe[title="Form 0"]')
+        .contentFrame()
+        .getByLabel("Company Name*")
+    ).toBeVisible();
+    await expect(
+      page1
+        .locator('iframe[title="Form 0"]')
+        .contentFrame()
+        .getByLabel("Average Daily Shipping Volume*")
+    ).toBeVisible();
+    await expect(
+      page1
+        .locator('iframe[title="Form 0"]')
+        .contentFrame()
+        .getByLabel("How Did You Hear About Us?*")
+    ).toBeVisible();
+    await expect(
+      page1
+        .locator('iframe[title="Form 0"]')
+        .contentFrame()
+        .getByText("Firstmile.com needs the")
+    ).toBeVisible();
+    await expect(
+      page1
+        .locator('iframe[title="Form 0"]')
+        .contentFrame()
+        .getByRole("button", { name: "Submit" })
+    ).toBeVisible();
+  });
 });
 
 test.describe("track a package", () => {
@@ -56,6 +139,9 @@ test.describe("multiple packages", () => {
     await expect(
       page.getByRole("button", { name: "Track Shipment" })
     ).toBeVisible();
+  });
+  test.fixme("can find packages", async ({ page }) => {
+    console.log("need a valid tracking number to test");
   });
 });
 
